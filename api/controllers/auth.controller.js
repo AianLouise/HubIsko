c2ac3f321e34a552fd6b28069129bf423eeaf4b3
AianLouise <aianlouisealfaro03@gmail.com>
2024-06-13 11:51:28 +0800

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword});
+    const newUser = new User({ username, email, password: hashedPassword });
     try {
         await newUser.save()
         res.status(201).json({ message: 'User created successfully' });
@@ -14,3 +15,33 @@ export const signup = async (req, res, next) => {
     }
 };
 
+export const signin = async (req, res, next) => {
+  // Destructure email and password from request body
+  const { email, password } = req.body;
+
+  try {
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler(404, 'User not found'));
+    }
+
+    // Check if password is correct
+    const isPasswordValid = bcryptjs.compareSync(password, user.password);
+    if (!isPasswordValid) {
+      return next(errorHandler(401, 'Invalid Credentials'));
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    // Exclude password from user data
+    const { password: userPassword, ...userData } = user._doc;
+
+    // Set cookie and send success response
+    res.cookie('access_token', token, { httpOnly: true }).status(200).json(userData);
+  } catch (error) {
+    // Pass error to error handler
+    next(error);
+  }
+};
