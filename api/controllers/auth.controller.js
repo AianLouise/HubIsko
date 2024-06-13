b3c49f4c8c656e16f03fbc7e2532f03bbe39f0ac
AianLouise <aianlouisealfaro03@gmail.com>
2024-06-13 18:59:15 +0800

    Create and add OAuth component

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,44 +4,76 @@ import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
-    try {
-        await newUser.save()
-        res.status(201).json({ message: 'User created successfully' });
-    } catch (error) {
-        next(error);
-    }
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const signin = async (req, res, next) => {
-  // Destructure email and password from request body
   const { email, password } = req.body;
-
   try {
-    // Check if user exists
-    const user = await User.findOne({ email });
-    if (!user) {
-      return next(errorHandler(404, 'User not found'));
-    }
-
-    // Check if password is correct
-    const isPasswordValid = bcryptjs.compareSync(password, user.password);
-    if (!isPasswordValid) {
-      return next(errorHandler(401, 'Invalid Credentials'));
-    }
-
-    // Generate JWT token
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    // Exclude password from user data
-    const { password: userPassword, ...userData } = user._doc;
-
-    // Set cookie and send success response
-    res.cookie('access_token', token, { httpOnly: true }).status(200).json(userData);
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'wrong credentials'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
   } catch (error) {
-    // Pass error to error handler
+    next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword, ...rest } = user._doc;
+      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+      res
+        .cookie('access_token', token, {
+          httpOnly: true,
+          expires: expiryDate,
+        })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          req.body.name.split(' ').join('').toLowerCase() +
+          Math.random().toString(36).slice(-8),
+        email: req.body.email,
+        password: hashedPassword,
+        profilePicture: req.body.photo,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword2, ...rest } = newUser._doc;
+      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+      res
+        .cookie('access_token', token, {
+          httpOnly: true,
+          expires: expiryDate,
+        })
+        .status(200)
+        .json(rest);
+    }
+  } catch (error) {
     next(error);
   }
 };
