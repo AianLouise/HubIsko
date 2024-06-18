4a390fc3bd32f752c654f5f48fdf133d8601cfc6
AianLouise <aianlouisealfaro03@gmail.com>
2024-06-18 21:31:04 +0800

    change the verificationUrl value on auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -38,7 +38,9 @@ export const signup = async (req, res, next) => {
       },
     });
 
-    const verificationUrl = `http://localhost:5173/verify-email?token=${emailVerificationToken}`;
+    // const verificationUrl = `http://localhost:5173/verify-email?token=${emailVerificationToken}`;
+
+    const verificationUrl = `https://hubisko.onrender.com//verify-email?token=${emailVerificationToken}`; 
 
     console.log('Sending verification email to:', email);
     await transporter.sendMail({
