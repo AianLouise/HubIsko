57ff5bfc63f9f0bb3c501059d6f3e317f766c782
Repxl <jrreyas@gmail.com>
2024-07-05 01:42:57 +0800

    Login Page and Google

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,6 +3,7 @@ import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
 import { useNavigate } from 'react-router-dom';
+import { FaGoogle } from "react-icons/fa";
 
 export default function OAuth() {
   const dispatch = useDispatch();
@@ -44,9 +45,10 @@ export default function OAuth() {
     <button
       type='button'
       onClick={handleGoogleClick}
-      className='bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95'
+      className='border rounded-lg p-3 hover:bg-slate-200 flex flex-row items-center justify-center gap-4 font-medium transition-colors ease-in-out'
     >
-      Continue with google
+      <FaGoogle className='text-blue-600' />
+      <span>Google Account</span>
     </button>
   );
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,6 +9,7 @@ import {
 import OAuth from '../components/OAuth';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { MdEmail } from "react-icons/md";
 import logo from '../assets/logo.png';
 
 
@@ -64,28 +65,44 @@ export default function SignIn() {
   };
 
   return (
-    <div className='flex flex-col md:flex-row items-center justify-center p-4 gap-10 min-h-screen'>
+    <div className='bg-[#f8f8fb] flex flex-col md:flex-row items-center text-leftp-4 gap-10 min-h-screen'>
       {/* Left Column for Logo or Image */}
-      <div className='flex-1 flex justify-center items-center'>
+      <div className='w-1/2 flex justify-center items-center'>
         <Link to='/'>
-          <img src={logo} alt="Logo" className="w-48 h-auto" />
+          <div className='bg-blue-600 w-56 h-56 rounded-lg'></div>
         </Link>
       </div>
 
       {/* Right Column for Sign In Form */}
-      <div className='flex flex-1 flex-col items-center justify-center'>
-        <h1 className='text-3xl text-center font-semibold my-7'>Login</h1>
+      <div className='w-1/2 flex items-center justify-center'>
+      <div className='flex flex-col items-start justify-center bg-white border rounded-md w-[600px]  h-[800px] shadow-md p-24'>
+        <div className='flex flex-col justify-center text-left gap-4 mb-8'>
+          <div className='flex flex-row gap-2 items-center mb-4'>
+            <div className='bg-blue-600 w-10 h-10 rounded-md'></div>
+            <span className='font-bold text-lg'>HubIsko</span>
+          </div>
+        <h1 className='text-4xl font-bold'>Log in to your Account</h1>
+        <span className='text-medium text-slate-500'>Welcome back! Please choose the login method:</span>
+        </div>
         <form onSubmit={handleSubmit} className='w-full max-w-md flex flex-col gap-4'>
+
+        <OAuth />
+        <div>
+        <div className='border-b-2 mt-6'></div>
+        <div className='text-center -translate-y-4'>
+          <span className='bg-white p-1 px-4 text-slate-500 font-medium '>  or continue with email </span>
+          </div>
+        </div>
           <input
             type='email'
             placeholder='Email'
             id='email'
             aria-label='Email'
             required
-            className='bg-slate-100 p-3 rounded-lg'
+            className='border bg-white p-3 rounded-lg focus:outline-blue-600'
             onChange={handleChange}
           />
-
+  
           <div className="relative">
             <input
               type={showPassword ? 'text' : 'password'}
@@ -93,7 +110,7 @@ export default function SignIn() {
               id='password'
               aria-label='Password'
               required
-              className='bg-slate-100 p-3 rounded-lg w-full'
+              className='border bg-white p-3 rounded-lg w-full focus:outline-blue-600'
               onChange={handleChange}
             />
             <button
@@ -106,7 +123,7 @@ export default function SignIn() {
           </div>
 
           {/* Forgot Password Link */}
-          <div className="text-right w-full">
+          <div className="text-right w-full mb-2">
             <Link to='/forgot-password' className='text-sm text-blue-500 hover:underline'>
               Forgot Password?
             </Link>
@@ -115,14 +132,14 @@ export default function SignIn() {
           <button
             type='submit'
             disabled={loading}
-            className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-50'
+            className='bg-blue-600 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-50'
           >
             {loading ? 'Loading...' : 'Login'}
           </button>
 
-          <OAuth />
+        
         </form>
-        <div className='flex gap-2 mt-5'>
+        <div className='flex w-full p-2 xl:px-20 gap-2 mt-5'>
           <p>Don't Have an account?</p>
           <Link to='/register' className='text-blue-500 hover:underline'>
             Register
@@ -133,6 +150,7 @@ export default function SignIn() {
             {error.message || 'Something went wrong!'}
           </p>
         )}
+        </div>
       </div>
     </div>
   );
