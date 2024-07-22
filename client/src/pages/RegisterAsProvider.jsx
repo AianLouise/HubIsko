19c26d987d8b4b56a2e8635ff63196a9bc37a6f0
AianLouise <aianlouisealfaro03@gmail.com>
2024-07-22 11:28:39 +0800

    Modal and Countdown function

--- client/src/pages/RegisterAsProvider.jsx
+++ client/src/pages/RegisterAsProvider.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaArrowRightLong } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 
@@ -11,6 +12,26 @@ export default function RegisterAsProvider() {
     password: '',
   });
 
+  const [showModal, setShowModal] = useState(false);
+  const [countdown, setCountdown] = useState(10); 
+
+  useEffect(() => {
+    let intervalId;
+    if (showModal && countdown > 0) {
+      intervalId = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+    } else if (countdown === 0) {
+      window.location.href = '/';
+    }
+    return () => clearInterval(intervalId); 
+  }, [showModal, countdown]);
+
+  const navigate = useNavigate();
+  const handleHome = () => {
+      navigate('/');
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData(prevState => ({
@@ -18,13 +39,20 @@ export default function RegisterAsProvider() {
       [name]: value,
     }));
   };
-
+  
+  const [isConfirmationVisible, setIsConfirmationVisible] = useState(true);
   const handleSubmit = (e) => {
     e.preventDefault();
     // Add form submission logic here
     // Ensure email uniqueness and password strength requirements
     console.log(formData);
+    setIsConfirmationVisible(false);
+    setShowModal(true);
+    setTimeout(() => {
+      navigate('/')
+    }, 10000);
   };
+
 
   const [activeButtonId, setActiveButtonId] = useState(1);
   const maxButtonId = 5; 
@@ -53,9 +81,18 @@ export default function RegisterAsProvider() {
       if (buttonId === activeButtonId) {
           return 'bg-white p-8 shadow rounded-md border';
       }else {
-          return 'hidden bg-white p-8 shadow rounded-md border';
+          return 'hidden';
       }
   };   
+
+  const handleHideButton = (buttonId) => {
+      if (buttonId === activeButtonId) {
+          return 'hidden';
+      } else {
+          return 'mt-4 bg-white hover:bg-slate-200 border shadow font-bold py-2 px-12 rounded';
+      }
+  };
+
   
   const getConfirmation = (buttonId) => {
     if (buttonId === activeButtonId) {
@@ -67,11 +104,13 @@ export default function RegisterAsProvider() {
 
   const getPrevorHome = (buttonId) => {
     if (buttonId === activeButtonId) {
-    return "Home";
+    return "mt-4 bg-white hover:bg-slate-200 border shadow font-bold py-2 px-12 rounded";
     } else {
-        return 'Previous';
+        return 'hidden mt-4 bg-white hover:bg-slate-200 border shadow font-bold py-2 px-12 rounded';
       }
-    }
+    };
+
+   
 
   return (
     <main className='bg-[#f8f8fb] font-medium flex flex-col items-center min-h-screen'>
@@ -187,7 +226,7 @@ export default function RegisterAsProvider() {
               <span>Text</span>
           </div>
 
-          <div className={getConfirmation(5)}>
+          <div className={isConfirmationVisible ? getConfirmation(5) : 'hidden'}>
               <div className='bg-white w-1/2 h-1/2 rounded-md p-4 font-medium'>
                   <span className='text-xl'>Please confirm the details</span>
 
@@ -197,13 +236,24 @@ export default function RegisterAsProvider() {
 
                   <div className='flex justify-between mt-10'>
                     <button onClick={handlePrevious} className='bg-white py-2 px-12 border rounded-md hover:bg-slate-200'>Go back</button>
-                    <button className='bg-blue-600 text-white py-2 px-12 rounded-md hover:bg-blue-800'>Submit</button>
+                    <button onClick={handleSubmit} className='bg-blue-600 text-white py-2 px-12 rounded-md hover:bg-blue-800'>Submit</button>
                   </div>
               </div>
           </div>
-          
+
+          {showModal && (
+              <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto bg-gray-900 bg-opacity-50">
+                <div className="bg-white flex-col gap-2 flex text-left p-8 w-1/4 shadow rounded-md border">
+                  <h1 className='text-4xl font-bold text-blue-600'>Congratulations!</h1>
+                  <span className='text-xl'>your request has been sent for approval! <br /></span>
+                  <span className='mt-2 text-slate-500'>We'll be redirecting you to the home page in <span className='text-blue-600 font-bold'>{countdown}</span> seconds.</span>
+                  </div>
+              </div>
+            )}
+                
           <div className='flex justify-between'>
-          <button onClick={handlePrevious} className="mt-4 bg-white hover:bg-slate-200 border shadow font-bold py-2 px-12 rounded">{getPrevorHome(1)}</button>
+          <button onClick={handleHome} className={getPrevorHome(1)}>Home</button>
+          <button onClick={handlePrevious} className={handleHideButton(1)}>Previous</button>
           <button onClick={handleNext} className="mt-4 bg-blue-500 hover:bg-blue-700 shadow text-white font-bold py-2 px-12 rounded">Next</button>
           </div>
       </div>
