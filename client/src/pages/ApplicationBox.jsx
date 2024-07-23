f3cbaae8f19901a96de91abd3e34af7eb933cb1e
Repxl <jrreyas@gmail.com>
2024-07-23 23:02:40 +0800

    Active State

--- client/src/pages/ApplicationBox.jsx
+++ client/src/pages/ApplicationBox.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
+import { FaAngleRight } from "react-icons/fa6";
 
 
 export default function ApplicationBox() {
@@ -11,12 +12,28 @@ export default function ApplicationBox() {
                 <main className="flex-grow bg-[#f8f8fb] font-medium">
 
                     <div className="max-w-6xl px-24 mx-auto">
+
+
+                        
+
                             <div className="border bg-white flex flex-col gap-8 rounded-md p-4 shadow mt-20">
                                <span className="text-xl">Application Inbox</span>
                                 
                            
                                 <div className="flex flex-col">
+                                    
+                                    {/* IF WALA */}
+                                    {/* <div className="text-center flex flex-col items-center gap-2">
+                                        <span className="text-slate-600">You have no applications yet</span>
+                                        <Link to={'/scholarship-listing'}>
+                                        <button className='flex gap-2 items-center bg-blue-600 rounded-md px-4 py-2 text-white fond-medium hover:bg-blue-800 group transition ease-in-out'>
+                                        Go to Scholarship List
+                                        <FaAngleRight className='w-5 h-5  group-hover:translate-x-2 transition ease-in-out' />
+                                        </button>
+                                        </Link>
+                                    </div> */}
 
+                                    {/* IF MERON */}
                                     <Link to={`/inboxed-application-detail`}>
                                     <div className="flex justify-between items-center hover:bg-slate-200 p-2 rounded-md">
                                     <div className="flex gap-2">
