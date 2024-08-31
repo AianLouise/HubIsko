57a1c785de159337df6206e3676f701776875fd8
AianLouise <aianlouisealfaro03@gmail.com>
2024-08-31 20:13:49 +0800

    Added Logo for provider and admin

--- client/src/components/ProviderHeaderAndSidebar.jsx
+++ client/src/components/ProviderHeaderAndSidebar.jsx
@@ -17,6 +17,7 @@ import { IoMenu } from "react-icons/io5";
 import { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import Logo from '../assets/NewLogoClean.png';
 
 
 export default function ProviderHeaderSidebar({ sidebarOpen, toggleSidebar, currentPath }) {
@@ -99,7 +100,7 @@ export default function ProviderHeaderSidebar({ sidebarOpen, toggleSidebar, curr
                     return (
                         <span key={index} className="text-lg font-bold text-blue-500">
                             /&nbsp;<Link to={routeTo}>{capitalizeWords(value)}</Link>
-                        </span> 
+                        </span>
                     );
                 })}
             </>
@@ -173,7 +174,7 @@ export default function ProviderHeaderSidebar({ sidebarOpen, toggleSidebar, curr
                 <aside className="fixed font-medium inset-y-0 left-0 transform translate-x-0 w-64 transition-transform duration-300 ease-in-out bg-white shadow-lg p-4 z-50">
                     <div className='flex justify-between mb-8'>
                         <div className='flex items-center gap-2'>
-                            <div className='bg-blue-600 w-6 h-6 rounded-md'></div>
+                            <img src={Logo} alt='Logo' className='w-6 h-6 rounded-md' />
                             <span className='font-bold text-blue-600 text-2xl'>HubIsko</span>
                         </div>
                         <button onClick={toggleSidebar} className='border border-blue-600 rounded-full items-center justify-center flex w-8 hover:bg-slate-200 transition hover:border-none'>
