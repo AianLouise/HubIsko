7a2dd3fe1a4b8c6078a62545c7a0a1da35916a32
Repxl <jrreyas@gmail.com>
2024-09-01 17:46:35 +0800

    Admin-Forums highlight function

--- client/src/components/AdminHeader.jsx
+++ client/src/components/AdminHeader.jsx
@@ -271,10 +271,10 @@ export default function AdminHeader({ sidebarOpen, toggleSidebar }) {
                             </li>
 
                             <li>
-                                <a href="/admin-forums" className="flex gap-2 items-center text-gray-800 hover:bg-blue-200 py-2 px-4 rounded-md">
-                                    <MdForum className="w-5 h-5 text-blue-600" /> {/* Updated icon */}
+                            <Link to={"/admin-forums"} className={`flex gap-2 items-center text-gray-800 hover:bg-blue-200 py-2 px-4 rounded-md ${location.pathname === '/admin-forums' ? 'bg-blue-600 text-white' : ''}`}>
+                                    <MdForum className={`w-5 h-5 text-blue-600 ${location.pathname =='/admin-forums' ? 'text-white':''}`} /> {/* Updated icon */}
                                     Forums
-                                </a>
+                            </Link>
                             </li>
 
                         </ul>
