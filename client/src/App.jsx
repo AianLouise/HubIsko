e2fc92ed7b61b4a91bdb38c0a959b840221e619d
Repxl <jrreyas@gmail.com>
2024-09-04 23:04:31 +0800

    Admin Settings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -61,6 +61,7 @@ import ProviderCreateForumPost from "./pages/Scholarship-Provider/ProviderCreate
 import AdminForums from "./pages/Admin/AdminForums";
 import AdminForumDetail from "./pages/Admin/AdminForumDetail";
 import AdminCreateForumPost from "./pages/Admin/AdminCreateForumPost";
+import AdminSettings from "./pages/Admin/AdminSettings";
 import ScholarshipDashboardDetails from "./pages/ScholarshipDashboardDetails";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
 import ScholarshipProgramDetails from "./pages/Admin/ScholarshipProgramApplicationDetails";
@@ -109,6 +110,7 @@ export default function App() {
         <Route element={<ProviderPrivateRoute allowedRoles={['admin']} />}>
         <Route element={<Layout />} path="/">
           <Route path="admin-dashboard" element={<AdminDashboard />} />
+          <Route path="admin-settings" element={<AdminSettings />} />
           <Route path="accounts" element={<Accounts />} />
           <Route path="students" element={<Students />} />
           <Route path="scholarship-provider" element={<ProviderAccounts />} />
--- client/src/components/AdminHeader.jsx
+++ client/src/components/AdminHeader.jsx
@@ -112,8 +112,11 @@ export default function AdminHeader({ sidebarOpen, toggleSidebar }) {
                         {dropdownOpen && (
                             <div className="absolute mt-2 right-0 bg-white text-gray-800 shadow-lg rounded-md p-2 w-52 z-50 font-medium">
                                 <ul>
-                                    <li className="p-2 hover:bg-gray-100 cursor-pointer">Profile</li>
-                                    <li className="p-2 hover:bg-gray-100 cursor-pointer">Settings</li>
+                                    <li className="p-2 hover:bg-gray-100 cursor-pointer">
+                                    <Link to={'/admin-settings'}>
+                                    Settings
+                                    </Link>
+                                    </li>
                                     <li className="p-2 hover:bg-gray-100 cursor-pointer" onClick={handleSignOut}>Sign out</li>
                                 </ul>
                             </div>
