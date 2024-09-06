fdde477ce5a08a07d654fb50fb215f4674d17403
Repxl <jrreyas@gmail.com>
2024-09-07 00:40:10 +0800

    Mobile Changes

--- client/src/pages/ScholarshipDashboardDetails.jsx
+++ client/src/pages/ScholarshipDashboardDetails.jsx
@@ -69,14 +69,20 @@ return (
                         <div className='flex flex-col gap-4'>
                             <div className='flex justify-between items-center gap-4'>
                                 <span className='text-2xl font-bold'>Announcements</span>
-                                <div className="flex gap-2 items-center bg-white shadow px-6 py-2 rounded-md border text-md">
+                                <div className="hidden lg:flex gap-2 items-center bg-white shadow px-6 py-2 rounded-md border text-md">
                                     <input type="text" 
                                     placeholder="Search Announcements" 
                                     className="w-full bg-transparent outline-none" />
                                 </div>
                             </div>
+
+                            <div className="lg:hidden flex gap-2 items-center bg-white shadow px-6 py-2 rounded-md border text-md">
+                                    <input type="text" 
+                                    placeholder="Search Announcements" 
+                                    className="w-full bg-transparent outline-none" />
+                                </div>
                             
-                            <div className='grid grid-cols-3 gap-10'> 
+                            <div className='grid lg:grid-cols-3 gap-10'> 
                                 <div className='bg-white border p-4 rounded-md flex flex-col gap-4 hover:-translate-y-1 hover:shadow-lg transition ease-in-out'>
 
                                     <div className='flex gap-2'>
