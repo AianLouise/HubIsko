bc7754f44cb686533bb5bcdcdf227f755839cab7
AianLouise <aianlouisealfaro03@gmail.com>
2024-11-11 20:15:59 +0800

    added * for required fields

--- client/src/components/ViewScholarshipDetails/EditScholarshipDetails.jsx
+++ client/src/components/ViewScholarshipDetails/EditScholarshipDetails.jsx
@@ -139,127 +139,127 @@ export default function EditScholarshipDetails() {
 
     const fieldsOfStudy = [
         "Open for All Courses",
-      "BA in Anthropology",
-    "BA in Art Studies",
-    "BA in Communication",
-    "BA in Development Communication",
-    "BA in Economics",
-    "BA in Fine Arts",
-    "BA in Geography",
-    "BA in History",
-    "BA in International Relations",
-    "BA in Journalism",
-    "BA in Law",
-    "BA in Linguistics",
-    "BA in Media Studies",
-    "BA in Religious Studies",
-    "BA in Social Work",
-    "BS in Accounting",
-    "BS in Accounting Information Systems",
-    "BS in Accounting Technology",
-    "BS in Aerospace Engineering",
-    "BS in Agribusiness Management",
-    "BS in Agricultural Engineering",
-    "BS in Agriculture",
-    "BS in Animal Science",
-    "BS in Applied Mathematics",
-    "BS in Applied Physics",
-    "BS in Architecture",
-    "BS in Astronomy",
-    "BS in Biochemistry",
-    "BS in Biology",
-    "BS in Biomedical Engineering",
-    "BS in Biosystems Engineering",
-    "BS in Business Administration",
-    "BS in Business Administration major in Financial Management",
-    "BS in Business Analytics",
-    "BS in Business Management",
-    "BS in Chemical Engineering",
-    "BS in Chemistry",
-    "BS in Civil Engineering",
-    "BS in Communication",
-    "BS in Computer Engineering",
-    "BS in Computer Science",
-    "BS in Criminology",
-    "BS in Cyber Security",
-    "BS in Data Analytics",
-    "BS in Data Science",
-    "BS in Dentistry",
-    "BS in Digital Media Arts",
-    "BS in Education",
-    "BS in Electrical Engineering",
-    "BS in Elementary Education",
-    "BS in Electronics and Communications Engineering",
-    "BS in Electronics Engineering",
-    "BS in Environmental Engineering",
-    "BS in Environmental Science",
-    "BS in Fashion Design",
-    "BS in Finance",
-    "BS in Food Science",
-    "BS in Food Technology",
-    "BS in Forensic Science",
-    "BS in Forestry",
-    "BS in Geodetic Engineering",
-    "BS in Geology",
-    "BS in Graphic Design",
-    "BS in Health Sciences",
-    "BS in Horticulture",
-    "BS in Hospitality Management",
-    "BS in Hotel and Restaurant Management",
-    "BS in Human Resource Development",
-    "BS in Industrial Design",
-    "BS in Industrial Engineering",
-    "BS in Information Systems",
-    "BS in Information Technology",
-    "BS in Internal Auditing",
-    "BS in Landscape Architecture",
-    "BS in Library and Information Science",
-    "BS in Management Accounting",
-    "BS in Marine Biology",
-    "BS in Marine Engineering",
-    "BS in Marine Transportation",
-    "BS in Marketing",
-    "BS in Materials Science",
-    "BS in Mathematics",
-    "BS in Mechanical Engineering",
-    "BS in Medical Laboratory Science",
-    "BS in Medical Technology",
-    "BS in Medicine",
-    "BS in Metallurgical Engineering",
-    "BS in Microbiology",
-    "BS in Midwifery",
-    "BS in Mining Engineering",
-    "BS in Molecular Biology",
-    "BS in Multimedia Arts",
-    "BS in Music",
-    "BS in Nanotechnology",
-    "BS in Nursing",
-    "BS in Nutrition",
-    "BS in Occupational Therapy",
-    "BS in Oceanography",
-    "BS in Pharmacy",
-    "BS in Physical Therapy",
-    "BS in Physics",
-    "BS in Political Science",
-    "BS in Psychology",
-    "BS in Public Health",
-    "BS in Radiologic Technology",
-    "BS in Real Estate Management",
-    "BS in Renewable/Sustainable Energy Engineering",
-    "BS in Robotics Engineering",
-    "BS in Secondary Education major in Biology",
-    "BS in Secondary Education major in Chemistry",
-    "BS in Secondary Education major in English",
-    "BS in Secondary Education major in General Sciences",
-    "BS in Secondary Education major in Mathematics",
-    "BS in Secondary Education major in Physics",
-    "BS in Social Work",
-    "BS in Sociology",
-    "BS in Software Engineering",
-    "BS in Statistics",
-    "BS in Tourism Management",
-    "BS in Veterinary Medicine",
-    "BS in Zoology"
+        "BA in Anthropology",
+        "BA in Art Studies",
+        "BA in Communication",
+        "BA in Development Communication",
+        "BA in Economics",
+        "BA in Fine Arts",
+        "BA in Geography",
+        "BA in History",
+        "BA in International Relations",
+        "BA in Journalism",
+        "BA in Law",
+        "BA in Linguistics",
+        "BA in Media Studies",
+        "BA in Religious Studies",
+        "BA in Social Work",
+        "BS in Accounting",
+        "BS in Accounting Information Systems",
+        "BS in Accounting Technology",
+        "BS in Aerospace Engineering",
+        "BS in Agribusiness Management",
+        "BS in Agricultural Engineering",
+        "BS in Agriculture",
+        "BS in Animal Science",
+        "BS in Applied Mathematics",
+        "BS in Applied Physics",
+        "BS in Architecture",
+        "BS in Astronomy",
+        "BS in Biochemistry",
+        "BS in Biology",
+        "BS in Biomedical Engineering",
+        "BS in Biosystems Engineering",
+        "BS in Business Administration",
+        "BS in Business Administration major in Financial Management",
+        "BS in Business Analytics",
+        "BS in Business Management",
+        "BS in Chemical Engineering",
+        "BS in Chemistry",
+        "BS in Civil Engineering",
+        "BS in Communication",
+        "BS in Computer Engineering",
+        "BS in Computer Science",
+        "BS in Criminology",
+        "BS in Cyber Security",
+        "BS in Data Analytics",
+        "BS in Data Science",
+        "BS in Dentistry",
+        "BS in Digital Media Arts",
+        "BS in Education",
+        "BS in Electrical Engineering",
+        "BS in Elementary Education",
+        "BS in Electronics and Communications Engineering",
+        "BS in Electronics Engineering",
+        "BS in Environmental Engineering",
+        "BS in Environmental Science",
+        "BS in Fashion Design",
+        "BS in Finance",
+        "BS in Food Science",
+        "BS in Food Technology",
+        "BS in Forensic Science",
+        "BS in Forestry",
+        "BS in Geodetic Engineering",
+        "BS in Geology",
+        "BS in Graphic Design",
+        "BS in Health Sciences",
+        "BS in Horticulture",
+        "BS in Hospitality Management",
+        "BS in Hotel and Restaurant Management",
+        "BS in Human Resource Development",
+        "BS in Industrial Design",
+        "BS in Industrial Engineering",
+        "BS in Information Systems",
+        "BS in Information Technology",
+        "BS in Internal Auditing",
+        "BS in Landscape Architecture",
+        "BS in Library and Information Science",
+        "BS in Management Accounting",
+        "BS in Marine Biology",
+        "BS in Marine Engineering",
+        "BS in Marine Transportation",
+        "BS in Marketing",
+        "BS in Materials Science",
+        "BS in Mathematics",
+        "BS in Mechanical Engineering",
+        "BS in Medical Laboratory Science",
+        "BS in Medical Technology",
+        "BS in Medicine",
+        "BS in Metallurgical Engineering",
+        "BS in Microbiology",
+        "BS in Midwifery",
+        "BS in Mining Engineering",
+        "BS in Molecular Biology",
+        "BS in Multimedia Arts",
+        "BS in Music",
+        "BS in Nanotechnology",
+        "BS in Nursing",
+        "BS in Nutrition",
+        "BS in Occupational Therapy",
+        "BS in Oceanography",
+        "BS in Pharmacy",
+        "BS in Physical Therapy",
+        "BS in Physics",
+        "BS in Political Science",
+        "BS in Psychology",
+        "BS in Public Health",
+        "BS in Radiologic Technology",
+        "BS in Real Estate Management",
+        "BS in Renewable/Sustainable Energy Engineering",
+        "BS in Robotics Engineering",
+        "BS in Secondary Education major in Biology",
+        "BS in Secondary Education major in Chemistry",
+        "BS in Secondary Education major in English",
+        "BS in Secondary Education major in General Sciences",
+        "BS in Secondary Education major in Mathematics",
+        "BS in Secondary Education major in Physics",
+        "BS in Social Work",
+        "BS in Sociology",
+        "BS in Software Engineering",
+        "BS in Statistics",
+        "BS in Tourism Management",
+        "BS in Veterinary Medicine",
+        "BS in Zoology"
     ];
 
     useEffect(() => {
@@ -300,7 +300,7 @@ export default function EditScholarshipDetails() {
                     <h2 className="text-2xl font-bold mb-4 border-b-2 pb-4">Scholarship Details</h2>
                     <div className='flex flex-col gap-4 px-4 space-y-1 mt-4'>
                         <div>
-                            <label className="block text-gray-700 font-semibold mb-2">Title of Scholarship</label>
+                            <label className="block text-gray-700 font-semibold mb-2">Title of Scholarship <span className='text-red-500'>*</span></label>
                             <p className="text-sm text-gray-500 mb-2">Please provide the official title of the scholarship program.</p>
                             <input
                                 type="text"
@@ -316,7 +316,7 @@ export default function EditScholarshipDetails() {
                         </div>
 
                         <div>
-                            <label className="block text-gray-700 font-semibold mb-2">Scholarship Description</label>
+                            <label className="block text-gray-700 font-semibold mb-2">Scholarship Description <span className='text-red-500'>*</span></label>
                             <p className="text-sm text-gray-500 mb-2">Please provide a detailed description of the scholarship program.</p>
                             <textarea
                                 name="description"
@@ -329,7 +329,7 @@ export default function EditScholarshipDetails() {
                         </div>
 
                         <div>
-                            <label className="block text-gray-700 font-semibold mb-2">Number of Scholarships Available</label>
+                            <label className="block text-gray-700 font-semibold mb-2">Number of Scholarships Available <span className='text-red-500'>*</span></label>
                             <p className="text-sm text-gray-500 mb-2">Please enter the total number of scholarship slots available.</p>
                             <input
                                 type="number"
@@ -344,7 +344,7 @@ export default function EditScholarshipDetails() {
                         </div>
 
                         <div>
-                            <label className="block text-gray-700 font-semibold mb-2">Scholarship Amount</label>
+                            <label className="block text-gray-700 font-semibold mb-2">Scholarship Amount <span className='text-red-500'>*</span></label>
                             <p className="text-sm text-gray-500 mb-2">Please enter the scholarship amount in PHP. Example: Up to PHP 10,000.</p>
                             <input
                                 type="text"
@@ -363,7 +363,7 @@ export default function EditScholarshipDetails() {
                     <h2 className="text-2xl font-bold mb-4 border-b-2 pb-4">Eligibility Criteria</h2>
                     <div className='flex flex-col gap-4 px-4 space-y-1 mt-4'>
                         <div>
-                            <label className="block text-gray-700 font-semibold mb-2">Education Level</label>
+                            <label className="block text-gray-700 font-semibold mb-2">Education Level <span className='text-red-500'>*</span></label>
                             <p className="text-sm text-gray-500 mb-2">Please select the appropriate education level for the scholarship program.</p>
                             <select
                                 name="educationLevel"
@@ -379,7 +379,7 @@ export default function EditScholarshipDetails() {
                         </div>
 
                         <div>
-                            <label className="block text-gray-700 font-semibold mb-2">Location</label>
+                            <label className="block text-gray-700 font-semibold mb-2">Location <span className='text-red-500'>*</span></label>
                             <p className="text-sm text-gray-500 mb-2">Select the location you want to provide.</p>
                             <select
                                 name="location"
@@ -400,7 +400,7 @@ export default function EditScholarshipDetails() {
                         </div>
 
                         <div>
-                            <label className="block text-gray-700 font-semibold mb-2">Field of Study</label>
+                            <label className="block text-gray-700 font-semibold mb-2">Field of Study <span className='text-red-500'>*</span></label>
                             <p className="text-sm text-gray-500 mb-2">Please select the field of study relevant to the scholarship program.</p>
                             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
                                 {fieldsOfStudy.map((field) => (
@@ -426,7 +426,7 @@ export default function EditScholarshipDetails() {
                 </div>
 
                 <div className='bg-white p-8 py-12 flex flex-col rounded-md border shadow'>
-                    <h2 className="text-2xl font-bold mb-4 border-b-2 pb-4">Scholarship Duration</h2>
+                    <h2 className="text-2xl font-bold mb-4 border-b-2 pb-4">Scholarship Duration <span className='text-red-500'>*</span></h2>
                     <div className='flex flex-col gap-4 px-4 space-y-1 mt-4'>
                         <div>
                             <label className="block text-gray-700">Duration</label>
@@ -557,7 +557,7 @@ export default function EditScholarshipDetails() {
                 </div>
 
                 <div className='bg-white p-8 py-12 flex flex-col rounded-md border shadow'>
-                    <h2 className="text-2xl font-bold mb-4 border-b-2 pb-4">Contact Information</h2>
+                    <h2 className="text-2xl font-bold mb-4 border-b-2 pb-4">Contact Information <span className='text-red-500'>*</span></h2>
                     <div className='flex flex-col gap-4 px-4 space-y-1 mt-4'>
                         <div>
                             <label className="block text-gray-700">Contact Email</label>
@@ -573,7 +573,7 @@ export default function EditScholarshipDetails() {
                         </div>
 
                         <div>
-                            <label className="block text-gray-700">Contact Phone Number</label>
+                            <label className="block text-gray-700">Contact Phone Number <span className='text-red-500'>*</span></label>
                             <p className="text-sm text-gray-500 mb-2">Please enter a valid phone number for contact purposes.</p>
                             <input
                                 type="tel"
